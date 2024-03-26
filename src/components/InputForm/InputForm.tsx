3019117ecb58ddbc037bd16be8bf04347393d0be
Alex Johnson <johnsonav1992@gmail.com>
2024-03-26 12:27:31 -0500

    Form is looking good

--- src/components/InputForm/InputForm.tsx
+++ src/components/InputForm/InputForm.tsx
@@ -1,44 +1,86 @@
+import { Formularity } from 'formularity';
+import { SteelElement } from '../../types/types';
 import {
-    FormStore
-    , Formularity
-} from 'formularity';
-import {
-    SteelElement
-    , SteelSpecFormValues
-} from '../../types/types';
-import {
-    Stack
+    Button
+    , Stack
     , TextField
+    , Typography
     , capitalize
 } from '@mui/material';
+import { formStore } from '../../App';
 
-type Props = {
-    formStore: FormStore<SteelSpecFormValues>;
-};
-
-const InputForm = ( { formStore }: Props ) => {
+const InputForm = () => {
     return (
         <Formularity
             formStore={ formStore }
-            useFormComponent={ false }
+            onSubmit={ values => console.log( values ) }
         >
             { ( {
                 initialValues
                 , Field
+                , SubmitButton
             } ) => (
                 <Stack gap='1rem'>
-                    {
-                        ( Object.keys( initialValues ) as SteelElement[] ).map( ( element => (
-                            <Field
-                                component={ TextField }
-                                label={ capitalize( element ) }
-                                type='number'
-                                key={ element }
-                                name={ element }
-                                size='small'
-                            />
-                        ) ) )
-                    }
+                    <Stack>
+                        <Typography variant='h6'>
+                            Initial Steel Batch Weight
+                        </Typography>
+                        <Field
+                            name='initialTotalWeight'
+                            type='number'
+                            placeholder='Enter weight of entire batch in kg'
+                            component={ TextField }
+                            size='small'
+                            InputProps={ { endAdornment: 'kg' } }
+                        />
+                    </Stack>
+                    <Stack
+                        direction='row'
+                        gap='1rem'
+                    >
+                        <Stack gap='1rem'>
+                            <Typography variant='h6'>
+                                Initial Specifications
+                            </Typography>
+                            {
+                                ( Object.keys( initialValues.initialSpec ) as SteelElement[] ).map( ( element => (
+                                    <Field
+                                        component={ TextField }
+                                        label={ capitalize( element ) }
+                                        type='number'
+                                        key={ element }
+                                        name={ `initialSpec.${ element }` }
+                                        size='small'
+                                        InputProps={ { endAdornment: '%' } }
+                                    />
+                                ) ) )
+                            }
+                        </Stack>
+                        <Stack gap='1rem'>
+                            <Typography variant='h6'>
+                                Final Specifications
+                            </Typography>
+                            {
+                                ( Object.keys( initialValues.finalSpec ) as SteelElement[] ).map( ( element => (
+                                    <Field
+                                        component={ TextField }
+                                        label={ capitalize( element ) }
+                                        type='number'
+                                        key={ element }
+                                        name={ `finalSpec.${ element }` }
+                                        size='small'
+                                        InputProps={ { endAdornment: '%' } }
+                                    />
+                                ) ) )
+                            }
+                        </Stack>
+                    </Stack>
+                    <SubmitButton
+                        component={ Button }
+                        variant='contained'
+                    >
+                        Submit
+                    </SubmitButton>
                 </Stack>
             ) }
         </Formularity>
