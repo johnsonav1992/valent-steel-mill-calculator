1e8b78940615d147fe443a4e04986a66d0375bb6
Alex Johnson <johnsonav1992@gmail.com>
2024-03-29 06:14:50 -0500

    submit handler move out

--- src/components/InputForm/InputForm.tsx
+++ src/components/InputForm/InputForm.tsx
@@ -1,6 +1,7 @@
 import { Formularity } from 'formularity';
 import {
     SteelElement
+    , SteelSpecFormValues
     , WeightsOutput
 } from '../../types/types';
 import {
@@ -37,40 +38,42 @@ const InputForm = ( {
         setIsWarningModalOpen( true );
     };
 
-    return (
-        <Formularity
-            formStore={ formStore }
-            onSubmit={ values => {
-                const isMissingAnyValue
+    const handleSubmit = ( values: SteelSpecFormValues ) => {
+        const isMissingAnyValue
                     = Object.entries( values.initialSpec ).some( ( [ , value ] ) => !value || Number( value ) < 0 )
                     || Object.entries( values.finalSpec ).some( ( [ , value ] ) => !value || Number( value ) < 0 );
 
-                const initialSpecPercent = Object.entries( values.initialSpec ).reduce( ( total, [ , value ] ) => { return total + Number( value ); }, 0 );
+        const initialSpecPercent = Object.entries( values.initialSpec ).reduce( ( total, [ , value ] ) => { return total + Number( value ); }, 0 );
 
-                const finalSpecPercent = Object.entries( values.finalSpec ).reduce( ( total, [ , value ] ) => { return total + Number( value ); }, 0 );
+        const finalSpecPercent = Object.entries( values.finalSpec ).reduce( ( total, [ , value ] ) => { return total + Number( value ); }, 0 );
 
-                const isNotCorrectTotalPercent = initialSpecPercent !== 100 || finalSpecPercent !== 100;
+        const isNotCorrectTotalPercent = initialSpecPercent !== 100 || finalSpecPercent !== 100;
 
-                if ( isMissingAnyValue ) {
-                    return openWarningModal( 'Please enter all values for each specification. All values must be greater than 0.' );
-                }
+        if ( isMissingAnyValue ) {
+            return openWarningModal( 'Please enter all values for each specification. All values must be greater than 0.' );
+        }
 
-                if ( isNotCorrectTotalPercent ) {
-                    return openWarningModal( 'The total percentage for each specification must be 100%.' );
-                }
+        if ( isNotCorrectTotalPercent ) {
+            return openWarningModal( 'The total percentage for each specification must be 100%.' );
+        }
 
-                if ( !values.initialTotalWeight ) {
-                    return openWarningModal( 'Please enter a weight for the initial batch.' );
-                }
+        if ( !values.initialTotalWeight ) {
+            return openWarningModal( 'Please enter a weight for the initial batch.' );
+        }
 
-                const weightCalculations = calculateWeightIncreases(
-                    convertSpecPercentages( values.initialSpec )
-                    , convertSpecPercentages( values.finalSpec )
-                    , Number( values.initialTotalWeight )
-                );
+        const weightCalculations = calculateWeightIncreases(
+            convertSpecPercentages( values.initialSpec )
+            , convertSpecPercentages( values.finalSpec )
+            , Number( values.initialTotalWeight )
+        );
 
-                setResult( weightCalculations );
-            } }
+        setResult( weightCalculations );
+    };
+
+    return (
+        <Formularity
+            formStore={ formStore }
+            onSubmit={ handleSubmit }
         >
             { ( {
                 initialValues
